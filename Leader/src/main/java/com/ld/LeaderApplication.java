0bd525345146577ad4b3a9d91e3305cccc33dd16
yunsinho <mallrege@naver.com>
2023-01-13 20:06:03 +0900

    3

--- Leader/src/main/java/com/ld/LeaderApplication.java
+++ Leader/src/main/java/com/ld/LeaderApplication.java
@@ -2,10 +2,16 @@ package com.ld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LeaderApplication {
+public class LeaderApplication  extends SpringBootServletInitializer{
 
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(LeaderApplication.class);
+	    }
 	public static void main(String[] args) {
 		SpringApplication.run(LeaderApplication.class, args);
 	}
