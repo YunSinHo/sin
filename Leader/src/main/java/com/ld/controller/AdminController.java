c215573ccaeee4ccba7164ded1f74ab37e09def5
yunsinho <mallrege@naver.com>
2023-01-13 18:56:05 +0900

    1

---
+++ Leader/src/main/java/com/ld/controller/AdminController.java
@@ -0,0 +1,14 @@
+package com.ld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {
+
+	@RequestMapping("/")
+	public String start() {
+		
+		return "index";
+	}
+}
